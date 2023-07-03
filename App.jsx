064479dc728f8a31fed64452abc3d657d6b9425d
Yuta Yamada <yutayamada@berkeley.edu>
2023-07-03 15:38:05 -0700

    added background

---
+++ App.jsx
@@ -0,0 +1,30 @@
+import Contact from "./src/components/Contact"
+import Hero from "./src/components/Hero"
+import Who from "./src/components/Who"
+import Works from "./src/components/Works"
+import styled from "styled-components"
+
+const Container = styled.div`
+    height: 100vh; 
+    scroll-snap-type : y mandatory; 
+    scroll-behavior: smooth;
+    overflow-y: auto;
+    scrollbar-width:none;
+    color: white; 
+    background: url("./img/bg.jpeg");
+    }
+`
+// &::-webkit-scrollbar{
+//     display: none;
+function App() {
+    return (
+      <Container>
+        <Hero/>
+        <Who/>
+        <Works/>
+        <Contact/>
+      </Container>
+    )
+  }
+  
+  export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Section = styled.div`
+    height: 100vh; 
+    scroll-snap-align: center;
+
+`
+
+const Hero = () => {
+    return (
+        <Section> Hero</Section>
+    )
+}
+
+export default Hero
