7994e6ab5fdd8a9ce8107b969b671cf4bfbad441
Yuta Yamada <yutayamada@berkeley.edu>
2023-07-03 16:13:19 -0700

    updated hero section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Section = styled.div`
+    display: flex;
+    justify-content: center; 
+`
+const Container = styled.div`
+    width: 1400px; 
+    display: flex; 
+    justify-content: space-between; 
+    align-items: center; 
+    padding: 10px 0px;
+`;
+const Links = styled.div`
+    display: flex;
+    align-items: center; 
+    gap: 50px;
+`;
+
+const Logo = styled.img`
+    height: 50px;
+`;
+
+const List = styled.ul`
+    display: flex; 
+    gap: 20px;
+    list-style: none;
+`; 
+
+const ListItem = styled.li`
+    cursor: pointer;
+`;
+
+const Icons = styled.div`
+    display: flex;
+    align-items: center; 
+    gap: 20px;
+`;
+
+const Icon = styled.img`
+    width: 20px; 
+    cursor: pointer; 
+`;
+
+const Button = styled.button`
+    width: 100px; 
+    padding: 10px;
+    background-color: #da4ea2;
+    cursor: pointer;
+    color: white;
+    border: none;
+    border-radius: 5px;
+`;
+
+
+const Navbar = () => {
+    return (
+        <Section> 
+            <Container>
+                <Links>
+                <Logo src="./img/logo.png"/>
+                <List>
+                    <ListItem> Home</ListItem>
+                    <ListItem> Studio</ListItem>
+                    <ListItem> Works</ListItem>
+                    <ListItem> Contact</ListItem>
+
+                </List>
+                </Links>
+                <Icons>
+                    <Icon src="./img/search.png"/>
+                    <Button> Hire Now</Button>
+                </Icons>
+            </Container>
+        </Section>
+    )
+}
+
+export default Navbar
