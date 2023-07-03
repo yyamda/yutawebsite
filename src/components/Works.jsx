cf1179393c0d20a242f53fbc1f36a884179a28ac
Yuta Yamada <yutayamada@berkeley.edu>
2023-07-03 16:43:41 -0700

    who section animation code

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,14 +1,91 @@
 import React from 'react'
 import styled from 'styled-components'
 
+
+const data = [
+    "Web Design",
+    "Development",
+    "Illustration", 
+    "Product Design", 
+    "Social Media",
+];
+
 const Section = styled.div`
     height: 100vh; 
     scroll-snap-align: center;
-`
+    display: flex;
+    justify-content: center;
+`;
+
+const Container = styled.div`
+    width: 1400px;
+    display: flex; 
+    justify-content: space-between; 
+`;
+
+const Left = styled.div`
+    flex: 1; 
+    display: flex;
+    align-items: center;
+`;
+
+const List = styled.ul`
+    list-style: none; 
+    display: flex; 
+    flex-direction: column; 
+    gap: 20px;
+`;
+
+const ListItem = styled.li`
+    font-size: 90px;
+    font-weight: bold; 
+    cursor: pointer; 
+    color: transparent; 
+    -webkit-text-stroke: 1px white;
+    position: relative; 
+
+    ::after{
+        content: "${(props)=>props.text}";
+        position: absolute;
+        top: 0;
+        left: 0;
+        color: pink;
+        width: 0px;
+        overflow: hidden; 
+        white-space: nowrap;
+    }
+
+    &:hover{
+        ::after{
+            animation: moveText 0.5s linear both;
+
+            @keyframes moveText {
+                to{
+                    width: 100%;
+                }
+            }
+        }
+    }
+`;
+
+const Right = styled.div`
+    flex: 1;
+`;
 
 const Works = () => {
     return (
-        <Section> Works</Section>
+        <Section> 
+            <Container>
+                <Left>
+                    <List>
+                        {data.map(item=>(
+                            <ListItem key={item} text={item}>{item}</ListItem>
+                        ))}
+                    </List>
+                </Left>
+                <Right></Right>
+            </Container>
+        </Section>
     )
 }
 
