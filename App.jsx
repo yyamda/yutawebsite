4406c1e83644e9c5af158301632b61c07dd7e47e
Yuta Yamada <yutayamada@berkeley.edu>
2023-07-03 19:41:13 -0700

    changing png access

--- App.jsx
+++ App.jsx
@@ -11,7 +11,7 @@ const Container = styled.div`
     overflow-y: auto;
     scrollbar-width:none;
     color: white; 
-    background: url("./img/bg.jpeg");
+    background: url("img/bg.jpeg");
     }
 `
 // &::-webkit-scrollbar{
